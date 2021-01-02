970b3224d03a88158ae873d163e19a8668ddb5d6
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-01-02 12:30:41 -0600

    added hoc example

--- src/hoc/components/hoc-page.js
+++ src/hoc/components/hoc-page.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {NormalForm} from './normal-form';
+import {MyFormAControlled} from './form-with-hoc';
 
 export const HocPage = () => {
   const handleNormalSubmit = data => alert(data);
+  const handleHocSubmit = data => alert(JSON.stringify(data));
   return (
     <>
       <h2>Ejemplo sin High Order Component (HOC)</h2>
@@ -11,6 +13,7 @@ export const HocPage = () => {
       <hr />
 
       <h2>Ejemplo con High Order Component (HOC)</h2>
+      <MyFormAControlled onSubmit={handleHocSubmit} />
     </>
   );
 };
